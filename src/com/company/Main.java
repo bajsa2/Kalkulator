6bd163e59dd33a463dd08f2f552043e55f0080fc
bajsa2 <bajsagarwolin@gmail.com>
2020-08-19 17:05:21 -0700

    ,

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,20 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        int a;
+        int b;
+
+        Scanner liczba1 = new Scanner(System.in);
+        Scanner liczba2 = new Scanner(System.in);
+        System.out.println("Podaj pierwsza liczbe:");
+        a = liczba1.nextInt();
+        System.out.println("Podaj druga liczbe:");
+        b = liczba2.nextInt();
+
     }
 }
