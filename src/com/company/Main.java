42874ade18ab3939303b0cc5cc734599896e3a1e
bajsa2 <bajsagarwolin@gmail.com>
2020-08-19 18:52:27 -0700

    ,

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,15 +6,48 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int a;
-        int b;
+        int firstNumber;
+        int secoundNumber;
+        int choice;
 
-        Scanner liczba1 = new Scanner(System.in);
-        Scanner liczba2 = new Scanner(System.in);
+
+        Scanner number1 = new Scanner(System.in);
+        Scanner number2 = new Scanner(System.in);
+        Scanner result = new Scanner(System.in);
         System.out.println("Podaj pierwsza liczbe:");
-        a = liczba1.nextInt();
+        firstNumber = number1.nextInt();
         System.out.println("Podaj druga liczbe:");
-        b = liczba2.nextInt();
+        secoundNumber = number2.nextInt();
+        System.out.println("Wybierz dzialanie");
+        System.out.println("1.Dodawanie");
+        System.out.println("2.Odejmowanie");
+        System.out.println("3.Mnozenie");
+        System.out.println("4.Dzielenie");
+        choice = result.nextInt();
 
+        switch (choice) {
+            case 1:
+                if (choice == 1)
+                    System.out.println("Wynik dodawania to = " + (firstNumber + secoundNumber));
+
+
+            case 2:
+                if (choice == 2)
+                    System.out.println("Wynik odejmowania to = " + (firstNumber - secoundNumber));
+
+
+            case 3:
+                if (choice == 3)
+                    System.out.println("Wynik mnozenia to = " + (firstNumber * secoundNumber));
+
+
+            case 4:
+                if (choice == 4)
+                    System.out.println("Wynik dzielenia to = " + (firstNumber / secoundNumber));
+
+
+
+
+      }
     }
 }
